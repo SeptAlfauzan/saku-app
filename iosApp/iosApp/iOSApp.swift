b8187e39db620d039c4010e896e96969a7394f48
septalfauzan <alfauzansepta@gmail.com>
2026-09-22 07:07:18 +0700

    feat: wire sentry dsn into ios xcconfig, infoplist, and app init

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
+    init() {
+        if let dsn = Bundle.main.object(forInfoDictionaryKey: "SENTRY_DSN") as? String {
+            SentryInitKt.initSentry(dsn: dsn)
+        }
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
